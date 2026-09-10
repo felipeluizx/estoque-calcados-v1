de74177f963dd69f04d84ae051b05f873138cceb
felipeluizx <felipe.luiz.e.maximo@gmail.com>
2026-09-10 20:19:45 -0300

    Atualizar versão das observações compactas

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -40,7 +40,7 @@ export async function onRequest(context) {
   if (isMainApp) {
     const gradeCss = '<link rel="stylesheet" href="/css/grade-order.css?v=20260910-2">';
     const refreshCss = '<link rel="stylesheet" href="/css/live-refresh-variation.css?v=20260910-3">';
-    const compactCss = '<link rel="stylesheet" href="/css/compact-details.css?v=20260910-2">';
+    const compactCss = '<link rel="stylesheet" href="/css/compact-details.css?v=20260910-3">';
     const groupingCss = '<link rel="stylesheet" href="/css/order-grouping.css?v=20260910-1">';
     const mobileUxCss = '<link rel="stylesheet" href="/css/mobile-ux-hardening.css?v=20260910-2">';
     const denseCss = '<link rel="stylesheet" href="/css/dense-customers-finance.css?v=20260910-1">';
@@ -55,7 +55,7 @@ export async function onRequest(context) {
     const gradeScript = '<script src="/js/grade-order.js?v=20260910-2" defer></script>';
     const refreshScript = '<script src="/js/live-refresh-variation.js?v=20260910-5" defer></script>';
     const deleteScript = '<script src="/js/delete-controls.js?v=20260910-1" defer></script>';
-    const compactScript = '<script src="/js/compact-details.js?v=20260910-2" defer></script>';
+    const compactScript = '<script src="/js/compact-details.js?v=20260910-3" defer></script>';
     const groupingScript = '<script src="/js/order-grouping.js?v=20260910-1" defer></script>';
     const mobileUxScript = '<script src="/js/mobile-ux-hardening.js?v=20260910-1" defer></script>';
     const denseScript = '<script src="/js/dense-customers-finance.js?v=20260910-1" defer></script>';
